c1e0d08b133daac678c316efc6234b9f3110b541
唐王瑶 <tangwangyao0228@163.com>
2024-07-18 22:14:56 +0800

    feat: text how to fix TS  errors

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,67 @@
  * @Description: some description
  * @FilePath: /DT-Web3-Interface/src/index.tsx
  */
-(function () {
-  function hello() {
-    return 'hello';
-  }
-  function world() {
-    return 'world';
-  }
-  const s = hello() + ' ' + world();
-  console.log(s, ':s');
-})();
+
+type DeepReadonly<T> = T extends Function
+  ? T // 如果 T 是函数，直接返回 T
+  : T extends Array<infer U>
+    ? ReadonlyArray<DeepReadonly<U>> // 如果 T 是数组，递归应用 DeepReadonly 于数组元素
+    : T extends object
+      ? { readonly [P in keyof T]: DeepReadonly<T[P]> } // 如果 T 是对象，递归应用 DeepReadonly 于每个属性
+      : T; // 其他情况，直接返回 T
+type cases = [
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>,
+];
+
+type X1 = {
+  a: () => 22;
+  b: string;
+  c: {
+    d: boolean;
+    e: {
+      g: {
+        h: {
+          i: true;
+          j: 'string';
+        };
+        k: 'hello';
+      };
+      l: [
+        'hi',
+        {
+          m: ['hey'];
+        },
+      ];
+    };
+  };
+};
+
+type X2 = { a: string } | { b: number };
+
+type Expected1 = {
+  readonly a: () => 22;
+  readonly b: string;
+  readonly c: {
+    readonly d: boolean;
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true;
+          readonly j: 'string';
+        };
+        readonly k: 'hello';
+      };
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey'];
+        },
+      ];
+    };
+  };
+};
+
+type Expected2 = { readonly a: string } | { readonly b: number };
+
+type r = DeepReadonly<X1>;
